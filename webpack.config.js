dbf2537b1e960c4c49a812999d0ddf8bdb0c0ca7
Nick Tallmon <thorbreakbeatgod@gmail.com>
2017-03-17 08:59:14 -0500

    build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'bin/bundle':'./src/index.js',
+
+    },
+    output: { path: './', filename: '[name].js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};

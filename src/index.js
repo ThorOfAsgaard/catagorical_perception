65edc0918e895274a914e0739f0d6ef5eacd4c3d
thor tallmon <thor@asgaardianworkshop.com>
2017-04-09 18:19:34 -0500

    foo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 
 
 ReactDOM.render(
-  <App soundPrefix="pi" numSounds="10" experimentOneRepetitions="2" fileSuffix=".wav" experimentTwoRepetitions="2" logo="./logo.svg"/>,
+  <App soundPrefix="pi" numSounds="10" experimentOneRepetitions="2" fileSuffix=".wav" experimentTwoRepetitions="5" logo="./logo.svg"/>,
   document.getElementById('root')
 );
